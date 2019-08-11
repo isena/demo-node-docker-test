dd2cf6dba39b0fae91fc4027ba2fcf8d999b07e6
Isaac Sena <isaac.sena@gmail.com>
2019-08-11 16:46:31 -0300

    Dockerizing integration tests

--- integration/features/support/env.js
+++ integration/features/support/env.js
@@ -1,3 +1,9 @@
 'use strict'
 
-exports.BASE_URL = 'http://localhost:5000'
+const host = process.env.APP_HOST || 'localhost'
+const port = process.env.APP_PORT || 8080
+const BASE_URL = `http://${host}:${port}`
+
+console.log(`BASE_URL: ${BASE_URL}`)
+
+exports.BASE_URL = BASE_URL
